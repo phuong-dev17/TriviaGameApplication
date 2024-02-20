96d02aecb9ef4011bdfbd3f5bdf5e5ce74c850fe
phuong-dev17 <125834793+phuong-dev17@users.noreply.github.com>
2024-02-20 14:23:44 +0100

    feat: implement start activity

--- app/src/main/java/com/quizgame/triviagameapplication/MainActivity.kt
+++ app/src/main/java/com/quizgame/triviagameapplication/MainActivity.kt
@@ -4,11 +4,23 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.quizgame.triviagameapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStartGame.setOnClickListener {
+            gotoStartGameActivity()
+        }
+    }
+
+    private fun gotoStartGameActivity() {
+        startActivity(StartGameActivity.intent(this))
+        finish()
     }
 
     companion object {
---
+++ app/src/main/java/com/quizgame/triviagameapplication/StartGameActivity.kt
@@ -0,0 +1,111 @@
+package com.quizgame.triviagameapplication
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import androidx.appcompat.widget.ListPopupWindow
+import com.quizgame.triviagameapplication.common.StartInfo
+import com.quizgame.triviagameapplication.databinding.ActivityStartGameBinding
+
+class StartGameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartGameBinding
+    private val categories = listOf<String>(
+        "music",
+        "sport and leisure",
+        "film and tv",
+        "arts and literature",
+        "history",
+        "society and culture",
+        "science",
+        "geography",
+        "food and drink",
+        "general knowledge"
+    )
+
+    private val difficulties = listOf<String>(
+        "easy",
+        "medium",
+        "hard"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.edtSelectCategory.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                createCategoryPopupWindow(categories)
+            }
+        }
+
+        binding.edtSelectDifficulty.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                createDifficultyPopupWindow(difficulties)
+            }
+        }
+
+            binding.btnStartGame.setOnClickListener {
+                val numberOfQuestion = binding.edtNumOfQuestions.text.toString().trim()
+                val category = binding.edtSelectCategory.text.toString().trim()
+                val difficulty = binding.edtSelectDifficulty.text.toString().trim()
+                if(numberOfQuestion.isNotEmpty() && numberOfQuestion.toIntOrNull() != null && category.isNotEmpty() && difficulty.isNotEmpty()) {
+                    Log.d("P123","CALLING START ACTIVITY")
+                    startActivity(
+                        QuestionActivity.intent(
+                            this, StartInfo(
+                                numberOfQuestion.toInt(),
+                                category,
+                               difficulty
+                            )
+                        )
+                    )
+                }
+            }
+        }
+
+        private fun createCategoryPopupWindow(items: List<String>) {
+            Log.d("P123", " CALLING POPUP WINDOWN")
+
+            val listPopupWindow = ListPopupWindow(this)
+            val adapter = ArrayAdapter(this, R.layout.menu_cell, items)
+            listPopupWindow.setAdapter(adapter)
+            listPopupWindow.width = 350
+            listPopupWindow.anchorView = binding.edtSelectCategory
+
+            listPopupWindow.setOnItemClickListener { _, _, position, _ ->
+                // Lấy nội dung dòng được chọn
+                val selectedItem = categories[position]
+                // Điền nội dung dòng được chọn vào EditText
+                binding.edtSelectCategory.setText(selectedItem)
+                listPopupWindow.dismiss()
+            }
+            listPopupWindow.show()
+        }
+
+        private fun createDifficultyPopupWindow(items: List<String>) {
+            val listPopupWindow = ListPopupWindow(this)
+            val adapter = ArrayAdapter(this, R.layout.menu_cell, items)
+            listPopupWindow.setAdapter(adapter)
+            listPopupWindow.anchorView = binding.edtSelectDifficulty
+
+            listPopupWindow.setOnItemClickListener { _, _, position, _ ->
+                val selectedItem = difficulties[position]
+                binding.edtSelectDifficulty.setText(selectedItem)
+                listPopupWindow.dismiss()
+            }
+            listPopupWindow.show()
+        }
+
+        companion object {
+            fun intent(context: Context): Intent {
+                return Intent(
+                    context,
+                    StartGameActivity::class.java
+                )
+            }
+        }
+    }
